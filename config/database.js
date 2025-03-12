51c2cea912ca1476a6b92b25ee599da6f0e3df95
Rifansyah13 <148674882+Rifansyah12@users.noreply.github.com>
2025-03-12 22:00:23 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 const db = new Sequelize('ilm_trip', 'postgres', 'Rakkha-17', {
-  host: 'localhost',
+  host: '82.29.197.69',
   dialect: 'postgres', // Menggunakan PostgreSQL
   logging: false, // Opsional: Untuk mematikan log SQL
 });
